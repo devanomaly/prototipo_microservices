804d42ec380e7f607a1117eb2b5bfd734b42b27a
devanomaly <rafaelfpcosta@gmail.com>
2021-02-09 11:49:08 -0300

    implementando event-bus

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,10 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import CommentCreate from './CommentCreate'
+import CommentList from './CommentList'
 
 export default function () {
   const [posts, setPosts] = useState({})
 
-  const fetchPosts = async() => {
+  const fetchPosts = async () => {
     const res = await axios.get('http://localhost:4000/posts')
 
     setPosts(res.data)
@@ -14,8 +16,20 @@ export default function () {
     fetchPosts()
   }, [])
 
-  console.log(posts);
+  const renderedPosts = Object.values(posts).map(post => {
+    return (
+      <div className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id} >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id}/>
+          <CommentCreate postId={post.id}/>
+        </div>
+      </div>
+    )
+  })
 
-return (<div />)
+  return (<div className="d-flex flex-row flex-wrap justify-content-between">{renderedPosts}</div>)
 
 }
